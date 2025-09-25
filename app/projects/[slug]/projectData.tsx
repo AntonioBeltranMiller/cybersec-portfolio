5807b036235b0febf75717a101da8af94e63b0dd
AntonioBeltranMiller <antonio.c.miller7@gmail.com>
2025-09-25 12:14:27 -0400

    Update and rename projectData.ts to projectData.tsx

--- app/projects/[slug]/projectData.tsx
+++ app/projects/[slug]/projectData.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Shield, AlertTriangle, Globe, Database, FileText, TrendingUp, Eye, Lock, Terminal, Search, Activity } from 'lucide-react'
 
 export interface Project {
